203a5fbffb9ea5428346d7441dc54bdab931772c
Denis Tyshchenko <denis.tyschenko@gmail.com>
2017-03-28 20:28:00 +0200

    Revisited BFS: Shortest Reach in a Graph with simplified solution

--- hackerrank/src/main/java/com/github/tyshchenko/algs4fun/hackerrank/ShortReachInAGraph.java
+++ hackerrank/src/main/java/com/github/tyshchenko/algs4fun/hackerrank/ShortReachInAGraph.java
@@ -2,113 +2,63 @@ package com.github.tyshchenko.algs4fun.hackerrank;
 
 import java.util.*;
 
-import static java.util.stream.Collectors.joining;
-
 /**
- * //FIXME: revisit, overcompicated solution
- *
  * <a href="https://www.hackerrank.com/challenges/ctci-bfs-shortest-reach">
  * BFS: Shortest Reach in a Graph</a>
- * <p>
+ *
  * Created by denis on 2/14/17.
  */
 public class ShortReachInAGraph {
 
     private static final int NOT_VISITED = -1;
+    private static final int NODE_DISTANCE = 6;
 
-    /**
-     * Controller method to accept input, convert input into multiple graphs,
-     * provide shortest path computation and create view of the result
-     *
-     * Such flow can be represented in terms of MVC pattern,
-     * however to be able to submit solution to hackerrank online verifier will use
-     * one single class decomposed on multiple utility methods.
-     */
-    public static String shortestPathsForMultiQueries(String input) {
-        Scanner sc = new Scanner(input);
-        int queriesNum = sc.nextInt();
-        StringBuilder output = new StringBuilder();
-        for (; queriesNum > 1; queriesNum--) {
-            output.append(shortestPathForQuery(sc))
-                    .append("\n");
-        }
-        return output.append(shortestPathForQuery(sc))
-                .toString();
-    }
+    public static class Graph {
 
-    private static String shortestPathForQuery(Scanner sc) {
-        List<List<Integer>> adjacency = readAdjacencyMatrix(sc);
-        int startNode = toNodeIndex(sc.nextInt());
-        int[] shortestPaths = shortestPathsFrom(startNode, adjacency);
-        return Arrays.stream(shortestPaths)
-                .mapToObj(String::valueOf)
-                .collect(joining(" "));
-    }
+        private final List<List<Integer>> adjacency;
+        private final int size;
 
-    static List<List<Integer>> readAdjacencyMatrix(Scanner sc) {
-        int nodes = sc.nextInt();
-        List<List<Integer>> adjacency = new ArrayList<>(nodes);
-
-        for (int i = 0; i < nodes; i++) {
-            adjacency.add(new ArrayList<>());
-        }
-
-        int edges = sc.nextInt();
-        for (int i = 0; i < edges; i++) {
-            int nodeA = toNodeIndex(sc.nextInt());
-            int nodeB = toNodeIndex(sc.nextInt());
-            adjacency.get(nodeA).add(nodeB);
-            adjacency.get(nodeB).add(nodeA);
-        }
-
-        return adjacency;
-    }
-
-    private static int toNodeIndex(int node) {
-        return node - 1;
-    }
-    /**
-     * @param startNode - node to compute shortest path to other n - 1 nodes.
-     *                  Path between two neighbours is 6 by task definition
-     * @return array of the shortest paths from startNode to all n - 1 other nodes
-     * or -1 if there is no connection with startNode
-     */
-    public static int[] shortestPathsFrom(int startNode, List<List<Integer>> adjacency) {
-        Queue<WeightedNode> queue =
-                putOnQueueTraversal(new LinkedList<>(), adjacency.get(startNode), 0);
-
-        int[] result = new int[adjacency.size() - 1];
-        Arrays.fill(result, NOT_VISITED);
-
-        while (!queue.isEmpty()) {
-            WeightedNode current = queue.poll();
-            int resultIndex = resultIndex(current.node, startNode);
-            if (current.node != startNode && result[resultIndex] == NOT_VISITED) {
-                putOnQueueTraversal(queue, adjacency.get(current.node), current.weight);
-                result[resultIndex] = current.weight;
+        public Graph(int size) {
+            this.size = size;
+            adjacency = new ArrayList<List<Integer>>(size);
+            for (int i = 0; i < size; i++) {
+                adjacency.add(new ArrayList<Integer>());
             }
         }
-        return result;
-    }
 
-    private static int resultIndex(int currentNodeIndex, int startNodeIndex) {
-        return currentNodeIndex > startNodeIndex ? currentNodeIndex - 1 : currentNodeIndex;
-    }
-
-    private static Queue<WeightedNode> putOnQueueTraversal(Queue<WeightedNode> queue, List<Integer> siblings, int weight) {
-        for (Integer node : siblings) {
-            queue.add(new WeightedNode(node, weight + 6));
+        public void addEdge(int first, int second) {
+            adjacency.get(first).add(second);
+            adjacency.get(second).add(first);
         }
-        return queue;
+
+        public int[] shortestReach(int startId) { // 0 indexed
+            int[] distances = new int[size];
+            Arrays.fill(distances, NOT_VISITED);
+
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(new Node(0, startId));
+
+            while (!queue.isEmpty()) {
+                Node currentNode = queue.poll();
+                if (distances[currentNode.id] == NOT_VISITED) {
+                    for (Integer adjacent : adjacency.get(currentNode.id)) {
+                        Node adjacentNode = new Node(currentNode.distance + NODE_DISTANCE, adjacent);
+                        queue.offer(adjacentNode);
+                    }
+                    distances[currentNode.id] = currentNode.distance;
+                }
+            }
+            return distances;
+        }
     }
 
-    private static class WeightedNode {
-        final Integer node;
-        final int weight;
+    public static class Node {
+        private final int distance;
+        private final int id;
 
-        public WeightedNode(Integer node, int weight) {
-            this.node = node;
-            this.weight = weight;
+        public Node(int distance, int id) {
+            this.distance = distance;
+            this.id = id;
         }
     }
 }
--- hackerrank/src/test/java/com/github/tyshchenko/algs4fun/hackerrank/ShortReachInAGraphTest.java
+++ hackerrank/src/test/java/com/github/tyshchenko/algs4fun/hackerrank/ShortReachInAGraphTest.java
@@ -6,14 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
 import java.util.Scanner;
-
-import static com.github.tyshchenko.algs4fun.hackerrank.ShortReachInAGraph.*;
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
-import static org.hamcrest.CoreMatchers.is;
 
 /**
  * Created by denis on 2/14/17.
@@ -21,86 +14,61 @@ import static org.hamcrest.CoreMatchers.is;
 @RunWith(JUnitParamsRunner.class)
 public class ShortReachInAGraphTest {
 
-    private static final List<List<Integer>> ADJACENCY_MATRIX = asList(
-            asList(1, 2),
-            singletonList(0),
-            singletonList(0),
-            emptyList()
-    );
-    private static final String FIRST_QUERY = "4 2\n" +
-            "1 2\n" +
-            "1 3\n";
-    public static final String SECOND_QUERY = "3 1\n" +
-            "2 3\n";
-    private static final String TWO_QUERY_INPUT =
-            "2\n" + /*num of queries */
-                    //first query
-                    FIRST_QUERY + "1\n" +
-                    //second query
-                    SECOND_QUERY + "2";
-
-    public static Object[][] shortestPathsData() {
-        return new Object[][]{
-                {0, singletonList(emptyList()), new int[]{}},
-                {1, asList(
-                        emptyList(),
-                        emptyList(),
-                        emptyList()
-                ), new int[]{-1, -1}},
-
-                {0, asList(
-                        asList(1, 2),
-                        singletonList(0),
-                        singletonList(0)
-                ), new int[]{6, 6}},
-
-                {0, asList(
-                        singletonList(1),
-                        asList(0, 2),
-                        singletonList(1)
-                ), new int[]{6, 12}},
-
-                {1, asList(
-                        singletonList(1),
-                        asList(0, 2),
-                        singletonList(1)
-                ), new int[]{6, 6}},
-
-                {2, asList(
-                        singletonList(1),
-                        asList(0, 2),
-                        singletonList(1)
-                ), new int[]{12, 6}},
-
-                {0, ADJACENCY_MATRIX, new int[]{6, 6, -1}}
-        };
-    }
-
-    public static Object[][] viewData() {
+    public static Object[] data() {
         return new Object[][]{
                 {
-                        TWO_QUERY_INPUT,
-                        "6 6 -1\n" +
-                                "-1 6"
+                        "4 2\n" +
+                                "1 2\n" +
+                                "1 3\n" +
+                                "1",
+                        new int[] {6, 6, -1}
                 },
+                {
+                    "3 1\n" +
+                            "2 3\n" +
+                            "2",
+                        new int[] {-1, 6}
+                }
         };
     }
 
     @Test
-    @Parameters(method = "shortestPathsData")
-    public void verifyShortestPathLookup(int startNode, List<List<Integer>> adjacency, int[] expectedPaths) {
-        Assert.assertThat(shortestPathsFrom(startNode, adjacency), is(expectedPaths));
+    @Parameters(method = "data")
+    public void verifyGraphQuery(String input, int[] expectedDistances) {
+        Scanner scanner = new Scanner(input);
+
+        // Create a graph of size n where each edge weight is 6:
+        ShortReachInAGraph.Graph graph = new ShortReachInAGraph.Graph(scanner.nextInt());
+        int m = scanner.nextInt();
+
+        // read and set edges
+        for (int i = 0; i < m; i++) {
+            int u = scanner.nextInt() - 1;
+            int v = scanner.nextInt() - 1;
+
+            // add each edge to the graph
+            graph.addEdge(u, v);
+        }
+
+        // Find shortest reach from node s
+        int startId = scanner.nextInt() - 1;
+        int[] actualDistances = graph.shortestReach(startId);
+
+        scanner.close();
+
+        Assert.assertArrayEquals(expectedDistances, exclude(startId, actualDistances));
     }
 
-    @Test
-    @Parameters(method = "viewData")
-    public void verifyShortestPathWithRepresentation(String input, String expected) {
-        Assert.assertThat(shortestPathsForMultiQueries(input), is(expected));
+    /**
+     * Added to test just for compatibility with hackerrank tests
+     */
+    public static int[] exclude(int index, int[] els) {
+        int[] result = new int[els.length - 1];
+        for (int i = 0; i < els.length - 1; i++) {
+            result[i] = i < index ? els[i] : els[i + 1];
+        }
+        return result;
     }
 
-    @Test
-    public void verifyConversionToAdjacencyMatrix() {
-        Scanner sc = new Scanner(FIRST_QUERY);
-        Assert.assertThat(readAdjacencyMatrix(sc), is(ADJACENCY_MATRIX ));
-    }
+
 }
