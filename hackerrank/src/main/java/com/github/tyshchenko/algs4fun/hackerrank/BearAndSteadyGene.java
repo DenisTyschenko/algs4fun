e9b9169f9c502ac9f28dd0a93d52276570aff98d
Denis Tyshchenko <denis.tyschenko@gmail.com>
2017-08-01 18:49:34 +0200

    Simplified "Bear And Steady Gene" implementation

--- hackerrank/src/main/java/com/github/tyshchenko/algs4fun/hackerrank/BearAndSteadyGene.java
+++ hackerrank/src/main/java/com/github/tyshchenko/algs4fun/hackerrank/BearAndSteadyGene.java
@@ -1,91 +1,55 @@
 package com.github.tyshchenko.algs4fun.hackerrank;
 
 
-import com.google.common.collect.ImmutableMap;
-
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 /**
  * <a href="https://www.hackerrank.com/challenges/bear-and-steady-gene">
  * Bear And Steady Gene</a>
- *
+ * <p>
  * Complexity is linear however input sequence will be traversed several (constant) times
- *
+ * <p>
  * Created by denis on 5/13/17.
  */
 public class BearAndSteadyGene {
-    private final static char OFFSET = 'A';
     private final static int NUCLEOTIDES = 4;
-    private final static Map<Character, Character> DECODER = ImmutableMap.<Character, Character>builder()
-            .put('A', 'A')
-            .put('C', 'B')
-            .put('T', 'C')
-            .put('G', 'D')
-            .build();
 
     /**
      * API method to be as close as possible to hackerrank input format
      */
     public static int minSubstringLengthToReplaceIn(String input) {
         Scanner sc = new Scanner(input);
-        char[] decoded = new char[input.length()];
-        char[] original = sc.next().toCharArray();
-        for (int i = 0; i < input.length(); i++) {
-            decoded[i] = DECODER.get(original[i]);
-        }
-        return minSubstringLengthToReplaceIn(decoded);
-    }
+        int length = sc.nextInt();
+        //only symbols above the limit are making gene non steady
+        int limit = length / NUCLEOTIDES;
+        Map<Character, Integer> charToFreq = new HashMap<>();
 
-    public static int minSubstringLengthToReplaceIn(char[] input) {
-        int N = input.length;
-        int limitNucleotideFreq = N / NUCLEOTIDES;
-        //count nucleotide frequencies
-        int[] inputFreq = new int[NUCLEOTIDES];
-        Arrays.fill(inputFreq, -limitNucleotideFreq);
-        // positive nucleotides are over the limit
-        for (int i = 0; i < input.length; i++) {
-            inputFreq[input[i] - OFFSET]++;
+        char[] genom = sc.next().toCharArray();
+        for (char symbol : genom) {
+            charToFreq.computeIfPresent(symbol, (k, freq) -> freq + 1);
+            charToFreq.putIfAbsent(symbol, 1);
         }
 
-        int[] sliceFreq = new int[NUCLEOTIDES];
-        //find first violating symbol in input seq to start with
-        int result = 0;
-        for (int lo = getBeginning(0, input, inputFreq), hi = lo;
-             lo >= 0 && hi < input.length; hi++) {
+        if (isSteady(charToFreq, limit)) {
+            return 0;
+        }
 
-            if (inputFreq[input[hi] - OFFSET] > 0) {
-                sliceFreq[input[hi] - OFFSET]++;
-            }
-
-            while (isSteady(inputFreq, sliceFreq) && lo <= hi) {
-                int length = hi - lo + 1;
-                result = result == 0 ? length : Math.min(result, length);
-
-                sliceFreq[input[lo] - OFFSET]--;
-                lo = getBeginning(lo + 1, input, inputFreq);
+        int result = Integer.MAX_VALUE;
+        for (int lo = 0, hi = lo; hi < length; hi++) {
+            charToFreq.compute(genom[hi], (k, freq) -> freq - 1);
+            while (isSteady(charToFreq, limit) && lo <= hi) {
+                result = Math.min(hi - lo + 1, result);
+                charToFreq.compute(genom[lo++], (k, freq) -> freq + 1);
             }
         }
         return result;
     }
 
-    private static boolean isSteady(int[] referenceFreq, int[] sliceFreq) {
-        for (int i = 0; i < NUCLEOTIDES; i++) {
-            if (sliceFreq[i] < referenceFreq[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static int getBeginning(int pos, char[] input, int[] pattern) {
-        for (int i = pos; i < input.length; i++) {
-            if (pattern[input[i] - OFFSET] > 0) {
-                return i;
-            }
-        }
-        return -1;
+    private static boolean isSteady(Map<Character, Integer> charToFreq, int limit) {
+        return charToFreq.values().stream()
+                .allMatch(freq -> freq <= limit);
     }
 }
 
